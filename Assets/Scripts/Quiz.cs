bebd8bb42b23d519dfa55080b67dcdd79aa8ec57
AndreD4 <andremai629@gmail.com>
2023-06-15 09:02:28 +0900

    sprite lights up correct image button

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
   [SerializeField] TextMeshProUGUI questionText;
   [SerializeField] QuestionSO question;
   [SerializeField] GameObject[] answersButton;
+  int correctAnswerIndex;
+  [SerializeField] Sprite defaultAnswerSprite;
+  [SerializeField] Sprite correctAnswerSprite;
 
 
   void Start()
@@ -18,6 +22,26 @@ public class Quiz : MonoBehaviour
     {
       TextMeshProUGUI buttonText = answersButton[i].GetComponentInChildren<TextMeshProUGUI>();
       buttonText.text = question.GetAnswer(i);
+    }
+  }
+
+  public void OnAnswerSelected(int index)
+  {
+    Image buttonImage;
+
+    if(index == question.GetCorrectAnsweIndex())
+    {
+      questionText.text = "correct";
+      buttonImage = answersButton[index].GetComponent<Image>();
+      buttonImage.sprite = correctAnswerSprite;
+    }
+    else
+    {
+      correctAnswerIndex = question.GetCorrectAnsweIndex();
+      string correctAnswer = question.GetAnswer(correctAnswerIndex);
+      questionText.text = "sorry, the correct answer was;\n" + correctAnswer;
+      buttonImage = answersButton[correctAnswerIndex].GetComponent<Image>();
+      buttonImage.sprite = correctAnswerSprite;
     }
   }
 
